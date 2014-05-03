5a617127d2d4ea9b415949d0f2ad82c787bee426
Jake Teton-Landis <just.1.jake@gmail.com>
2014-05-03 02:48:55 -0700

    brew on the right branch & get data

--- reqs.sh
+++ reqs.sh
@@ -15,9 +15,20 @@ git clone "https://github.com/justjake/linuxbrew" "$VIRTUAL_ENV/linuxbrew"
 add-bundle-to-path "$VIRTUAL_ENV/linuxbrew"
 
 # install brew packages
-cd "$BREW"
+pushd "$BREW"
 git checkout add-phonetisaurus
+popd
 
 brew install openfst --with-fsts-options
 brew install MITLM m2m-aligner
 brew install cmu-pocketsphinx cmuclmtk phonetisaurus
+
+# get data
+wget http://phonetisaurus.googlecode.com/files/g014b2b.tgz
+tar -zxf g014b2b.tgz
+pushd g014b2b
+./compile-fst.sh
+popd
+
+mkdir -p data
+mv g014b2b data/
